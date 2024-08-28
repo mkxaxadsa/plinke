41bd121677f9f29715241f520f786cbe8b47c2f0
ivan-lab-pvls <ivan.labasok@pvls.tech>
2024-08-28 13:15:29 +0200

    full complete without OneSignal

---
+++ ios/OneSignalNotificationServiceExtension/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  OneSignalNotificationServiceExtension
+//
+//  Created by kilxsx on 28/8/24.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
